ee3a4aab880e4060b4a9f72db8efc74164743c28
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2026-06-23 13:29:45 -1000

    OOP Test 2: Method Dispatch Test (Lecture 32, Example 5)

--- oop-programs/MethodDispatchTest.kt
+++ oop-programs/MethodDispatchTest.kt
@@ -1,32 +1,33 @@
-open class C1 {
+open class C1Dispatch {
     open fun initialize() {}
-    
+
     open fun m1(): Int = m2()
-    
+
     open fun m2(): Int = 13
 }
 
-open class C2 : C1() {
+open class C2Dispatch : C1Dispatch() {
     override fun m1(): Int = 22
-    
+
     override fun m2(): Int = 23
-    
+
     fun m3(): Int = super.m1()
 }
 
-class C3 : C2() {
+class C3Dispatch : C2Dispatch() {
     override fun m1(): Int = 32
-    
+
     override fun m2(): Int = 33
 }
 
 fun main() {
-    val o3 = C3()
+    val o3 = C3Dispatch()
     val result = o3.m3()
-    
+
     println("o3.m3() result: $result")
     println("\nExplanation:")
     println("o3.m3() calls super.m1()")
-    println("super.m1() is C2.m1(), which returns 22")
-    println("Even though o3 is C3 instance, super constrains dispatch to C2")
+    println("Even with super.m1(), the actual instance (C3Dispatch) is used")
+    println("So C3Dispatch.m1() is called, which returns 33")
+    println("This demonstrates Dynamic Method Dispatch in Kotlin")
 }
