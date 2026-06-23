a5383314cecbef23fb4ddee597322358cb93f2e8
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2026-06-23 09:40:05 -1000

    Initialize TodoApp with task management options
    First commit

---
+++ oop-programs/TodoApp.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val tasks = mutableListOf<String>()
+
+    while (true) {
+        println("\n=== Todo List ===")
+        println("1: Add task")
+        println("2: View tasks")
+        println("3: Delete task")
+        println("4: Exit")
+        print("Choose (1-4): ")
+    }
