0d1e08a787a5b901b5972fb96bf65d1078f1dd2f
RAITO OSHIRO <133190036+Raito0046@users.noreply.github.com>
2026-06-23 13:28:16 -1000

    OOP Test 1: Variable Inheritance Test (Lecture 32, Example 3)

--- oop-programs/VariableInheritanceTest.kt
+++ oop-programs/VariableInheritanceTest.kt
@@ -1,28 +1,28 @@
 open class C1 {
-    var x: Int = 0
-    var y: Int = 0
-    
+    open var x: Int = 0
+    open var y: Int = 0
+
     open fun initialize() {}
-    
+
     fun setx1(v: Int) {
         x = v
     }
-    
+
     fun sety1(v: Int) {
         y = v
     }
-    
+
     fun getx1(): Int = x
     fun gety1(): Int = y
 }
 
 class C2 : C1() {
-    var y: Int = 0  // Shadows parent's y
-    
+    override var y: Int = 0  // Override parent's y
+
     fun sety2(v: Int) {
         this.y = v
     }
-    
+
     fun getx2(): Int = x
     fun gety2(): Int = this.y
 }
@@ -32,11 +32,11 @@ fun main() {
     o2.setx1(101)
     o2.sety1(102)
     o2.sety2(999)
-    
+
     println("getx1(): ${o2.getx1()}")
     println("gety1(): ${o2.gety1()}")
     println("getx2(): ${o2.getx2()}")
     println("gety2(): ${o2.gety2()}")
-    
+
     println("\nResult: [${o2.getx1()}, ${o2.gety1()}, ${o2.getx2()}, ${o2.gety2()}]")
 }
